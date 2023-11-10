51ea24f43ce1054c3066fd8fb23b7fbfe5a11c18
0-0snow0-0 <juliana-ort@hotmail.fr>
2023-11-10 12:57:53 -0500

    Creation of Classe proprio

---
+++ Exercice_maison_2/Exercice_maison_2/Proprietaire.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Exercice_maison_2
+{
+    internal class Proprietaire
+    {
+        public string Nom { get; set; }
+        public double Prenom { get; set; }
+
+        public override string ToString()
+        {
+            return $"{Nom} - {Prenom}";
+        }
+    }
+}
